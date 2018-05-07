ccd3b1b6684841bd3b4efd0cbf6acbeafcd0da44
Kuldeep-jindani <jindanikuldeep@gmail.com>
2018-05-07 17:14:52 +0530

    category image dynamic

--- app/src/main/java/com/place/market/pro/sau/marketplace/Adapters/BottomNav_CategoryList_Adapter.java
+++ app/src/main/java/com/place/market/pro/sau/marketplace/Adapters/BottomNav_CategoryList_Adapter.java
@@ -6,31 +6,47 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.place.market.pro.sau.marketplace.R;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class BottomNav_CategoryList_Adapter extends RecyclerView.Adapter<BottomNav_CategoryList_Adapter.ViewHolder> {
 
-Context context;
-JSONArray array;
+    Context context;
+    JSONArray array;
 
-    public BottomNav_CategoryList_Adapter(Context context,JSONArray array) {
+    public BottomNav_CategoryList_Adapter(Context context, JSONArray array) {
         this.context = context;
-        this.array=array;
+        this.array = array;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-       View view= LayoutInflater.from(context).inflate(R.layout.category_image_layout,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.category_image_layout, parent, false);
 
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        try {
+            JSONObject obj=array.getJSONObject(position);
+
+
+            Glide.with(context).load("http://kisanunnati.com/market_place/public/uploads/"+obj.getString("image")).into(holder.cate_img);
+
+            holder.category_id.setText(obj.getString("id"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -41,9 +57,13 @@ JSONArray array;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        TextView category_id;
+        ImageView cate_img;
+
         public ViewHolder(View itemView) {
             super(itemView);
-
+            cate_img = itemView.findViewById(R.id.cate_img);
+            category_id = itemView.findViewById(R.id.category_id);
 
         }
     }
